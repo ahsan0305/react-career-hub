2591014bdf2c44654c8b4b15e74d5ffa914a142f
ahsan0305 <hridoahsanul@gmail.com>
2023-09-24 17:58:53 +0600

    Implement Job filter

--- src/Components/AppliedJobs/AppliedJobs.jsx
+++ src/Components/AppliedJobs/AppliedJobs.jsx
@@ -9,6 +9,25 @@ const AppliedJobs = () => {
 
     const [appliedJobs, setAppliedJobs] = useState([]);
 
+    // this handle for filter by 
+    const [ displayJobs , setDisplayJobs] = useState([]);
+
+    
+    // this handle for filter by 
+    const handleJobsFilter = filter => {
+        if(filter === 'all'){
+            setDisplayJobs(appliedJobs);
+        }
+        else if( filter === 'remote'){
+            const remoteJobs = appliedJobs.filter(job => job.remote_or_onsite === 'Remote');
+            setDisplayJobs(remoteJobs);
+        }
+        else if(filter === 'onsite'){
+            const onsiteJobs = appliedJobs.filter(job => job.remote_or_onsite === 'Onsite');
+            setDisplayJobs(onsiteJobs);
+        }
+    }
+
 
     useEffect(() => {
         const storedJobIds = getStoreJobApplication();
@@ -25,30 +44,35 @@ const AppliedJobs = () => {
             // const jobsApplied = jobs.filter(job => storedJobIds.includes(job.id));
 
             // console.log(jobs, storedJobIds, jobsApplied); 
-            setAppliedJobs(jobsApplied)
+            setAppliedJobs(jobsApplied);
+            setDisplayJobs(jobsApplied);
         }
-    }, [])
+    }, [jobs])
 
     return (
         <div>
             <h3 className="text-3xl">Applied Jobs: {appliedJobs.length}</h3>
-
+            {/* start for dropdown btn */}
             <div>
                 <div className="dropdown dropdown-left">
                     <label tabIndex={0} className="btn m-1">Filter By</label>
                     <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><a>All Types</a></li>
-                        <li><a>Remote</a></li>
-                        <li><a>On site</a></li>
+                        <li onClick={()=> handleJobsFilter('all')}><a>All Types</a></li>
+                        <li onClick={()=> handleJobsFilter('remote')}><a>Remote</a></li>
+                        <li onClick={()=> handleJobsFilter('onsite')}><a>Onsite</a></li>
                     </ul>
                 </div>
             </div>
-
+            {/* end for dropdown btn */}
+            
             <ul>
                 {
-                    appliedJobs.map(job => <li key={job.id}><span>{job.job_title}  {job.company_name}  :: {job.remote_or_onsite}</span></li>)
+                   displayJobs.map(job => <li key={job.id}><span>{job.job_title}  {job.company_name}  :: {job.remote_or_onsite}</span></li>)
                 }
             </ul>
+
+            {/* Start for card section  */}
+            
 
         </div>
     );
